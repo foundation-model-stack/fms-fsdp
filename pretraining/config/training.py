240a2748a647139f616aeaa83b631dfffe67a2ac
Andrea Frittoli <andrea.frittoli@gmail.com>
2024-02-07 11:13:15 +0000

    Automatic code reformat by black
    Signed-off-by: Andrea Frittoli <andrea.frittoli@gmail.com>

--- pretraining/config/training.py
+++ pretraining/config/training.py
@@ -16,7 +16,9 @@ class train_config:
     data_path: str = "/lustre/data"
     seq_length: int = 4096
     sep_token: int = 1
-    datasets: str = "lang=en/dataset=commoncrawl,lang=en/dataset=webhose,lang=en/dataset=github_clean,lang=de/dataset=wikipedia,lang=es/dataset=wikipedia,lang=fr/dataset=wikipedia,lang=ja/dataset=wikipedia,lang=pt/dataset=wikipedia,lang=en/dataset=wikimedia,lang=en/dataset=uspto,lang=en/dataset=pubmedcentral,lang=en/dataset=arxiv,lang=en/dataset=stackexchange,lang=en/dataset=PG19"
+    datasets: str = (
+        "lang=en/dataset=commoncrawl,lang=en/dataset=webhose,lang=en/dataset=github_clean,lang=de/dataset=wikipedia,lang=es/dataset=wikipedia,lang=fr/dataset=wikipedia,lang=ja/dataset=wikipedia,lang=pt/dataset=wikipedia,lang=en/dataset=wikimedia,lang=en/dataset=uspto,lang=en/dataset=pubmedcentral,lang=en/dataset=arxiv,lang=en/dataset=stackexchange,lang=en/dataset=PG19"
+    )
     weights: str = "7700,500,550,28,17,22,25,8,100,500,175,250,100,25"
     logical_shards: int = 768
 
