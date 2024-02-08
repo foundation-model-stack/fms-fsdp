b83f0f47d8f966a85f761f33f25cccedf88dafa5
Davis Wertheimer <dww78@cornell.edu>
2024-02-08 16:10:24 -0500

    Remove modeling_utils as init is now default
    We can add this back later once we add support for principled auto-scaling initialization

--- pretraining/utils/model_utils.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-
-def post_init_original(model):
-    # Override existing fms model init scheme to use the hardcoded one from Llama
-    for m in model.modules():
-        if isinstance(m, nn.Linear) or isinstance(m, nn.Embedding):
-            m.weight.data.normal_(0, 0.02)
-    return model
