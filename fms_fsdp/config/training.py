1d0396b6fcf274d6e09827f9b03c98201d771a14
Linsong Chu <lchu@us.ibm.com>
2024-04-11 15:07:55 -0400

    update default configs

--- fms_fsdp/config/training.py
+++ fms_fsdp/config/training.py
@@ -6,43 +6,43 @@ from typing import Optional, Union
 class train_config:
     # model
     model_variant: str = "7b"
-    ckpt_load_path: str = "/lustre/pretrain/ckpt"
-    ckpt_save_path: str = "/lustre/pretrain/ckpt"
+    ckpt_load_path: str = "/fsx/output/ckpt"
+    ckpt_save_path: str = "/fsx/output/ckpt"
 
     # dataset and dataloader
     use_dummy_dataset: bool = False
-    data_path: str = "/lustre/data"
+    data_path: str = "/fsx/data"
+    datasets: str = "lang=en/dataset=commoncrawl,lang=en/dataset=webhose,lang=en/dataset=github_clean,lang=de/dataset=wikipedia,lang=es/dataset=wikipedia,lang=fr/dataset=wikipedia,lang=ja/dataset=wikipedia,lang=pt/dataset=wikipedia,lang=en/dataset=wikimedia,lang=en/dataset=uspto,lang=en/dataset=pubmedcentral,lang=en/dataset=arxiv,lang=en/dataset=stackexchange"
+    weights: str = "7725,500,550,28,17,22,25,8,100,500,175,250,100"
     seq_length: int = 4096
     sep_token: int = 1
-    datasets: str = "lang=en/dataset=commoncrawl,lang=en/dataset=webhose,lang=en/dataset=github_clean,lang=de/dataset=wikipedia,lang=es/dataset=wikipedia,lang=fr/dataset=wikipedia,lang=ja/dataset=wikipedia,lang=pt/dataset=wikipedia,lang=en/dataset=wikimedia,lang=en/dataset=uspto,lang=en/dataset=pubmedcentral,lang=en/dataset=arxiv,lang=en/dataset=stackexchange,lang=en/dataset=PG19"
-    weights: str = "7700,500,550,28,17,22,25,8,100,500,175,250,100,25"
-    logical_shards: int = 768
+    logical_shards: int = 1024
 
     # fsdp policies
-    mixed_precision: bool = True
+    sharding_strategy: str = "hsdp"
     fsdp_activation_checkpointing: bool = False
     selective_checkpointing: Union[float, str] = 1  # percentage of blocks to apply ac
-    sharding_strategy: str = "hsdp"
+    mixed_precision: bool = True
     low_cpu_fsdp: bool = False
 
     # training spec
-    seed: int = 2023
     batch_size: int = 2
-    num_steps: int = 2000000
+    num_steps: int = 1000000
     learning_rate: float = 3e-4
     grad_clip_thresh: float = 1.0
+    seed: int = 2023
 
     # profiling
     use_profiler: bool = False
     profiler_rank0_only: bool = True
 
     # logging
-    report_interval: int = 200
-    checkpoint_interval: int = 20000
+    report_interval: int = 100
+    checkpoint_interval: int = 10000
     tracker: Optional[str] = None  # None, "wandb", "aim"
-    tracker_dir: str = "/lustre/lchu/fms-fsdp"
+    tracker_dir: str = "/fsx/aim_logs/llama"
     tracker_project_name: str = "llama"  # project name for a group of runs
     tracker_run_id: Optional[str] = None  # run id, for job resume purpose
 
     # compile
-    use_torch_compile: bool = False
+    use_torch_compile: bool = True
