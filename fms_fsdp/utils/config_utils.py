5887896cf18c0a878e9f514d4b7f4703adba9c05
Davis Wertheimer <dww78@cornell.edu>
2024-07-22 10:48:34 -0400

    Remove tele configs

--- fms_fsdp/utils/config_utils.py
+++ fms_fsdp/utils/config_utils.py
@@ -100,26 +100,6 @@ def get_model_config(model_variant):
             hidden_grow_factor=3.5,
             max_expected_seq_len=4096,
         )
-    elif model_variant == "llama3_1.8b_tele16":
-        llama_config = LLaMAConfig(
-            src_vocab_size=128256,
-            emb_dim=2048,
-            nheads=32,
-            kvheads=2,
-            nlayers=24,
-            hidden_grow_factor=3.75,
-            max_expected_seq_len=4096,
-        )
-    elif model_variant == "llama3_1.8b_tele4":
-        llama_config = LLaMAConfig(
-            src_vocab_size=128256,
-            emb_dim=2048,
-            nheads=16,
-            kvheads=4,
-            nlayers=24,
-            hidden_grow_factor=3.5,
-            max_expected_seq_len=4096,
-        )
     elif model_variant == "llama3_70b":
         llama_config = LLaMAConfig(
             src_vocab_size=128256,
