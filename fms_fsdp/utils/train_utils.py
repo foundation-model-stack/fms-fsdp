8d6cbd5af658ef440cd8282f7a85cfb6b96ed286
Linsong Chu <lchu@us.ibm.com>
2024-08-06 12:36:51 -0400

    up

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -87,8 +87,8 @@ def train(
         optimizer.zero_grad()
         output = model(input)
         output = output.logits if hasattr(output, "logits") else output
-        ce_loss = torch.nn.CrossEntropyLoss()
-        loss = ce_loss(output.view(-1, output.size(-1)), label.view(-1).long())
+        ce_loss = torch.nn.MSELoss()
+        loss = ce_loss(output, label)
 
         loss.backward()
         ddp_stats[1] += model.clip_grad_norm_(cfg.grad_clip_thresh).item()
