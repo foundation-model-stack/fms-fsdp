0817ddcace669bd20771c01664b09b243ede4189
Linsong Chu <lchu@us.ibm.com>
2024-08-10 09:50:42 -0400

    add quick debug

--- fms_fsdp/utils/checkpointing_utils.py
+++ fms_fsdp/utils/checkpointing_utils.py
@@ -24,6 +24,7 @@ def get_latest(targdir, qualifier=lambda x: True):
     """Fetch the latest file or folder written to target directory, subject to name passing the qualifier fn.
     If directory is empty or nonexistent or no items qualify, return None."""
     if os.path.exists(targdir) and len(os.listdir(targdir)) > 0:
+        print(targdir)
         latest = max(
             [
                 os.path.join(targdir, x)
