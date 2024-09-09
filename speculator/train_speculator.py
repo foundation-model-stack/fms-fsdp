5636f347d299144d02da5b59db0c79faad5a8d3f
sahil suneja <sahilsuneja@gmail.com>
2024-09-09 15:13:28 +0000

    black fix
    Signed-off-by: sahil suneja <sahilsuneja@gmail.com>
    Signed-off-by: Joshua Rosenkranz <jmrosenk@us.ibm.com>

--- speculator/train_speculator.py
+++ speculator/train_speculator.py
@@ -132,7 +132,9 @@ def main(**kwargs):
         speculator_mesh = None
     else:
         base_model_mesh = dist.device_mesh.init_device_mesh(
-            "cuda", (world_size // cfg.tp_size, cfg.tp_size), mesh_dim_names=("dp", "tp")
+            "cuda",
+            (world_size // cfg.tp_size, cfg.tp_size),
+            mesh_dim_names=("dp", "tp"),
         )
         speculator_mesh = dist.device_mesh.init_device_mesh("cuda", (world_size,))
         torch._C._distributed_c10d._register_process_group(
