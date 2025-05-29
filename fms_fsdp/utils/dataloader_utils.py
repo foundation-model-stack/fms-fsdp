6d751e57ab62532094292151b1746a0ace4e004c
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-05-29 15:32:30 -0400

    Expose doc breakpoint in cfg
    Signed-off-by: Davis Wertheimer <davis.wertheimer@ibm.com>

--- fms_fsdp/utils/dataloader_utils.py
+++ fms_fsdp/utils/dataloader_utils.py
@@ -1,4 +1,5 @@
 import torch
+from math import ceil
 
 from fms_fsdp.utils.dataset_utils import (
     ArrowHandler,
@@ -104,6 +105,7 @@ def get_data_loader(cfg, rank, world_size):
         bos_token=cfg.bos_token,
         strip_tokens=set(droplist),
         min_length=3,
+        max_consecutive_chunks=ceil(cfg.doc_breakpoint/1024),
         seed=cfg.seed,
     )
     # Add rescaling/resharding
