bb97ccfd20520d42625609f227493c490cb412fa
Davis Wertheimer <dww78@cornell.edu>
2025-06-04 21:51:44 -0400

    flash attn

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -85,7 +85,7 @@ def train(
         label = label.to(local_rank)
 
         optimizer.zero_grad()
-        output = model(input, attn_algorithm="math")
+        output = model(input, attn_algorithm="flash")
         output = output.logits if hasattr(output, "logits") else output
         ce_loss = torch.nn.CrossEntropyLoss()
         loss = ce_loss(output.view(-1, output.size(-1)), label.view(-1).long())
